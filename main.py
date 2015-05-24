e1bcfb0b180f729274994809e9da91776e690a7c
Snifferdog <snifferdog97@gmail.com>
2015-05-23 23:18:51 -0500

    Added simple dice rolling

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Welcome to Dice Simulator 2015!")
+import random
+
+def roll():
+    side = random.randrange(1, 6)
+    return side
+
+def startup():
+    print("Welcome to Dice Simulator 2015!")
+    raw_input("Press enter to roll...")
+    print(roll())
+
+startup()
+
+
+
