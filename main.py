d4fee3e3e6b5b149b926e7fa699941bc5c2f6468
Snifferdog <snifferdog97@gmail.com>
2015-05-23 23:29:25 -0500

    Added dice icons

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import random
 
 def roll():
-    return random.randrange(1, 6)
+    side = random.randrange(1, 6)
+    if side == 1:
+        print("|---|")
+        print("|-1-|")
+        print("|---|")
+    if side == 2:
+        print("|---|")
+        print("|-2-|")
+        print("|---|")
+    if side == 3:
+        print("|---|")
+        print("|-3-|")
+        print("|---|")
+    if side == 4:
+        print("|---|")
+        print("|-4-|")
+        print("|---|")
+    if side == 5:
+        print("|---|")
+        print("|-5-|")
+        print("|---|")
+    if side == 6:
+        print("|---|")
+        print("|-6-|")
+        print("|---|")
 
 def startup():
     print("Welcome to Dice Simulator 2015!")
     raw_input("Press enter to roll...")
-    print(roll())
+    roll()
 
 startup()
 
